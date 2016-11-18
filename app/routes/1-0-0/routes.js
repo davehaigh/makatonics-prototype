008eb390e11714e76ceaf116526073b9fd35ff3d
Dave Haigh <dave.haigh@digital.hmrc.gov.uk>
2016-11-18 13:22:27 +0000

    First commit

---
+++ app/routes/1-0-0/routes.js
@@ -0,0 +1,14 @@
+module.exports = function(router) {
+    
+    // CHANGE ME TO THE VERSION YOURE WORKING ON
+    var version = "1-0-0"
+
+    // router.get('/' + version + '/home-redirect', function(req, res) {
+    //    res.redirect(301, "/" + version + "/index") ;
+    // });
+
+    // router.post('/' + version + '/known-facts', function (req, res) {
+    //     res.redirect(301, '/' + version + '/select-user');
+    // });
+
+};
