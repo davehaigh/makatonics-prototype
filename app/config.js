b28bd9495a6dc81892fd5c58aff038283e775362
Dave Haigh <dave.haigh@digital.hmrc.gov.uk>
2016-11-20 12:54:03 +0000

    added v1 of makatonics prototype

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Makatonics',
+  serviceName: 'Makaton translator',
 
   // Default port that prototype runs on
   port: '3000',
--- app/routes/1-0-0/routes.js
+++ app/routes/1-0-0/routes.js
@@ -3,9 +3,13 @@ module.exports = function(router) {
     // CHANGE ME TO THE VERSION YOURE WORKING ON
     var version = "1-0-0"
 
-    // router.get('/' + version + '/home-redirect', function(req, res) {
-    //    res.redirect(301, "/" + version + "/index") ;
-    // });
+    router.get('/' + version + '/makaton-gallery-redirect', function(req, res) {
+       res.redirect(301, "/" + version + "/makaton-gallery") ;
+    });
+
+    router.get('/' + version + '/index-redirect', function(req, res) {
+       res.redirect(301, "/" + version + "/index") ;
+    });
 
     // router.post('/' + version + '/known-facts', function (req, res) {
     //     res.redirect(301, '/' + version + '/select-user');
